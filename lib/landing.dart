e3fd3191c2c42109228eb23bd244b45c42ce9a36
Sam Evans <smlevans@gmail.com>
2019-09-06 17:47:19 -1000

    app and main files

---
+++ lib/landing.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(
+				title: Text('Hello'),
+			),
+			body: Text('This is some text. Welcome to my app')
+		);
+	}
+}
